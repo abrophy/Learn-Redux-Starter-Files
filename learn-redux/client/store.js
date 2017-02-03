833515ee443cb5aaab6e09b285267bfd232abf33
Arthur Brophy <artbrophy@gmail.com>
2017-02-03 15:23:10 -0500

    completed video #19

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -14,7 +14,12 @@ const defaultState = {
     comments,
 };
 
-const store = createStore(rootReducer, defaultState);
+// add redux dev tools enhancer to store
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
