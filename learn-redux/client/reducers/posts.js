edae28995b84fecb2e4e3c0432a2dc66dc7dae33
Arthur Brophy <artbrophy@gmail.com>
2017-02-02 22:43:38 -0500

    completed video #9

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -3,8 +3,10 @@
 // 2. copy of current state
 
 function posts(state = [], action) {
+    console.log("the post will change");
     console.log(state,action);
     return state;
 }
 
 export default posts;
+
